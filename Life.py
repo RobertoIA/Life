1d0285796ebe0ec3a96963ad982a5b8551a525d5
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-07-30 15:08:17 +0200

    Implemented 'Seeds' cellular automaton.
    Added interface to select automaton.

--- Life.py
+++ Life.py
@@ -17,7 +17,15 @@ class Board(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.canvas = Canvas(parent, width=WIDTH, height=HEIGHT, bd=-2)
-        self.canvas.pack()
+        self.canvas.grid(row=0, column=0)
+
+        self.automaton = StringVar()
+        self.automaton.set("life")
+        self.life = Radiobutton(parent, text="Conway's game of life", variable=self.automaton, value="life")
+        self.life.grid(row=1, column=0, sticky=W)
+
+        self.seeds = Radiobutton(parent, text="Seeds", variable=self.automaton, value="seeds")
+        self.seeds.grid(row=2, column=0, sticky=W)
 
     def draw_cell(self, position):
         # rectangle in x1 y1 x2 y2
@@ -53,46 +61,17 @@ class Life(object):
         self.animate()
 
     def animate(self):
-        self.game_of_life()
+        if self.board.automaton.get() == "life":
+            self.game_of_life()
+        elif self.board.automaton.get() == "seeds":
+            self.seeds()
         self.parent.after(DELAY, self.animate)
 
     def game_of_life(self):
         new_cells = copy.deepcopy(self.cells)
         for x in range(0, WIDTH / CELL_SIZE):
             for y in range(0, HEIGHT / CELL_SIZE):
-                neighbours = 0
-                # north
-                if y != 0:
-                    if self.cells[(x, y - 1)]:
-                        neighbours += 1
-                # north-east
-                if x < WIDTH / CELL_SIZE - 1 and y != 0:
-                    if self.cells[(x + 1, y - 1)]:
-                        neighbours += 1
-                # north-west
-                if x != 0 and y != 0:
-                    if self.cells[(x - 1, y - 1)]:
-                        neighbours += 1
-                # south
-                if y < HEIGHT / CELL_SIZE - 1:
-                    if self.cells[(x, y + 1)]:
-                        neighbours += 1
-                # south-east
-                if x < WIDTH / CELL_SIZE - 1 and y < HEIGHT / CELL_SIZE - 1:
-                    if self.cells[(x + 1, y + 1)]:
-                        neighbours += 1
-                # south-west
-                if x != 0 and y < HEIGHT / CELL_SIZE - 1:
-                    if self.cells[(x - 1, y + 1)]:
-                        neighbours += 1
-                # east
-                if x < WIDTH / CELL_SIZE - 1:
-                    if self.cells[(x + 1, y)]:
-                        neighbours += 1
-                # west
-                if x != 0:
-                    if self.cells[(x - 1, y)]:
-                        neighbours += 1
+                neighbours = self.count_neighbours(x, y)
 
                 if self.cells[(x, y)]:
                     # under-population
@@ -113,6 +92,58 @@ class Life(object):
                         self.board.turn_black((x, y))
 
         self.cells = new_cells
+
+    def seeds(self):
+        new_cells = copy.deepcopy(self.cells)
+        for x in range(0, WIDTH / CELL_SIZE):
+            for y in range(0, HEIGHT / CELL_SIZE):
+                neighbours = self.count_neighbours(x, y)
+
+                if not self.cells[(x, y)] and neighbours == 2:
+                    new_cells[(x, y)] = True
+                    self.board.turn_black((x, y))
+                else:
+                    new_cells[(x, y)] = False
+                    self.board.turn_white((x, y))
+
+        self.cells = new_cells
+
+    def count_neighbours(self, x, y):
+        neighbours = 0
+        # north
+        if y != 0:
+            if self.cells[(x, y - 1)]:
+                neighbours += 1
+            # north-east
+        if x < WIDTH / CELL_SIZE - 1 and y != 0:
+            if self.cells[(x + 1, y - 1)]:
+                neighbours += 1
+            # north-west
+        if x != 0 and y != 0:
+            if self.cells[(x - 1, y - 1)]:
+                neighbours += 1
+            # south
+        if y < HEIGHT / CELL_SIZE - 1:
+            if self.cells[(x, y + 1)]:
+                neighbours += 1
+            # south-east
+        if x < WIDTH / CELL_SIZE - 1 and y < HEIGHT / CELL_SIZE - 1:
+            if self.cells[(x + 1, y + 1)]:
+                neighbours += 1
+            # south-west
+        if x != 0 and y < HEIGHT / CELL_SIZE - 1:
+            if self.cells[(x - 1, y + 1)]:
+                neighbours += 1
+            # east
+        if x < WIDTH / CELL_SIZE - 1:
+            if self.cells[(x + 1, y)]:
+                neighbours += 1
+            # west
+        if x != 0:
+            if self.cells[(x - 1, y)]:
+                neighbours += 1
+
+        return neighbours
 
 if __name__ == "__main__":
     root = Tk()
