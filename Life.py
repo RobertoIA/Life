728ca6ed2790e2fe6be18c7c24e39ba3fb4a342d
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-07-31 08:21:25 +0200

    Various optimizations.

--- Life.py
+++ Life.py
@@ -2,17 +2,17 @@ from Tkinter import *
 from random import randint
 import copy
 
-WIDTH = 301
-HEIGHT = 301
+WIDTH = 401
+HEIGHT = 401
 CELL_SIZE = 4
-DELAY = 30
+DELAY = 10
 CELL_DENSITY = 7
 
 
 class Board(Frame):
     """Grid area"""
 
-    tiles = dict()
+    cells = list()
 
     def __init__(self, parent):
         Frame.__init__(self, parent)
@@ -28,37 +28,34 @@ class Board(Frame):
         self.seeds = Radiobutton(parent, text="Seeds", variable=self.automaton, value="seeds", indicatoron=0)
         self.seeds.grid(row=2, column=0, sticky=W + E)
 
-    def draw_cell(self, position):
-        # rectangle in x1 y1 x2 y2
-        self.tiles[position] = self.canvas.create_rectangle(position[0] * CELL_SIZE,
-                                                            position[1] * CELL_SIZE,
-                                                            (position[0] + 1) * CELL_SIZE,
-                                                            (position[1] + 1) * CELL_SIZE, fill="white")
+    def init_cells(self):
+        for x in range(0, WIDTH / CELL_SIZE):
+            column = list()
+            for y in range(0, HEIGHT / CELL_SIZE):
+                color = "white"
+                if randint(1, 100) >= 100 - CELL_DENSITY:
+                    color = "black"
+                column.append(self.canvas.create_rectangle(x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE,
+                                                            (y + 1) * CELL_SIZE, fill=color))
 
-    def turn_white(self, position):
-        self.canvas.itemconfigure(self.tiles[position], fill="white")
+            self.cells.append(column)
 
-    def turn_black(self, position):
-        self.canvas.itemconfigure(self.tiles[position], fill="black")
+    def turn_white(self, x, y):
+        self.canvas.itemconfigure(self.cells[x][y], fill="white")
+
+    def turn_black(self, x, y):
+        self.canvas.itemconfigure(self.cells[x][y], fill="black")
+
+    #TODO en commit separado: get_color, toggle_color
 
 
 class Life(object):
     """Main game loop."""
 
-    cells = dict()
-
     def __init__(self, parent):
-        """Initial game conditions"""
         self.parent = parent
         self.board = Board(parent)
-        for x in range(0, WIDTH / CELL_SIZE):
-            for y in range(0, HEIGHT / CELL_SIZE):
-                self.cells[(x, y)] = False
-                self.board.draw_cell((x, y))
-                if randint(1, 100) >= 100 - CELL_DENSITY:
-                    self.cells[(x, y)] = True
-                    self.board.turn_black((x, y))
-
+        self.board.init_cells()
         self.animate()
 
     def animate(self):
@@ -66,82 +63,78 @@ class Life(object):
             self.game_of_life()
         elif self.board.automaton.get() == "seeds":
             self.seeds()
+
         self.parent.after(DELAY, self.animate)
 
     def game_of_life(self):
-        new_cells = copy.deepcopy(self.cells)
+        canvas = self.board.canvas
         for x in range(0, WIDTH / CELL_SIZE):
             for y in range(0, HEIGHT / CELL_SIZE):
                 neighbours = self.count_neighbours(x, y)
 
-                if self.cells[(x, y)]:
+                if canvas.itemcget(self.board.cells[x][y], "fill") == "black":
                     # under-population
                     if neighbours < 2:
-                        new_cells[(x, y)] = False
-                        self.board.turn_white((x, y))
+                        self.board.turn_white(x, y)
                     # survival
                     elif neighbours == 2 or neighbours == 3:
                         pass
                     # overcrowding
                     else:
-                        new_cells[(x, y)] = False
-                        self.board.turn_white((x, y))
+                        self.board.turn_white(x, y)
                 else:
                     # reproduction
                     if neighbours == 3:
-                        new_cells[(x, y)] = True
-                        self.board.turn_black((x, y))
-
-        self.cells = new_cells
+                        self.board.turn_black(x, y)
 
     def seeds(self):
-        new_cells = copy.deepcopy(self.cells)
+        canvas = self.board.canvas
+        new_cells = copy.deepcopy(self.board.cells)
         for x in range(0, WIDTH / CELL_SIZE):
             for y in range(0, HEIGHT / CELL_SIZE):
                 neighbours = self.count_neighbours(x, y)
 
-                if not self.cells[(x, y)] and neighbours == 2:
-                    new_cells[(x, y)] = True
-                    self.board.turn_black((x, y))
+                if canvas.itemcget(self.board.cells[x][y], "fill") == "white" and neighbours == 2:
+                    self.board.turn_black(x, y)
                 else:
-                    new_cells[(x, y)] = False
-                    self.board.turn_white((x, y))
+                    self.board.turn_white(x, y)
 
-        self.cells = new_cells
+        self.board.cells = new_cells
 
     def count_neighbours(self, x, y):
+        canvas = self.board.canvas
         neighbours = 0
         # north
         if y != 0:
-            if self.cells[(x, y - 1)]:
+            if canvas.itemcget(self.board.cells[x][y - 1], "fill") == "black":
                 neighbours += 1
             # north-east
         if x < WIDTH / CELL_SIZE - 1 and y != 0:
-            if self.cells[(x + 1, y - 1)]:
+            if canvas.itemcget(self.board.cells[x + 1][y - 1], "fill") == "black":
                 neighbours += 1
             # north-west
         if x != 0 and y != 0:
-            if self.cells[(x - 1, y - 1)]:
+            if canvas.itemcget(self.board.cells[x - 1][y - 1], "fill") == "black":
                 neighbours += 1
             # south
         if y < HEIGHT / CELL_SIZE - 1:
-            if self.cells[(x, y + 1)]:
+            if canvas.itemcget(self.board.cells[x][y + 1], "fill") == "black":
                 neighbours += 1
             # south-east
         if x < WIDTH / CELL_SIZE - 1 and y < HEIGHT / CELL_SIZE - 1:
-            if self.cells[(x + 1, y + 1)]:
+            if canvas.itemcget(self.board.cells[x + 1][y + 1], "fill") == "black":
                 neighbours += 1
             # south-west
         if x != 0 and y < HEIGHT / CELL_SIZE - 1:
-            if self.cells[(x - 1, y + 1)]:
+            if canvas.itemcget(self.board.cells[x - 1][y + 1], "fill") == "black":
                 neighbours += 1
             # east
         if x < WIDTH / CELL_SIZE - 1:
-            if self.cells[(x + 1, y)]:
+            if canvas.itemcget(self.board.cells[x + 1][y], "fill") == "black":
                 neighbours += 1
             # west
         if x != 0:
-            if self.cells[(x - 1, y)]:
+            if canvas.itemcget(self.board.cells[x - 1][y], "fill") == "black":
                 neighbours += 1
 
         return neighbours
